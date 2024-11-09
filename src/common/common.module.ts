c1ca79ca05af4c4ecb40eaa7b1148ba8a0a3652e
Duvan <duvanriano2626@gmail.com>
2024-11-09 13:25:25 -0500

    Se crea un recurso completo common para poder crear el DTO que se va a encargar de la paginacion la paginacion varia segun si se esta trabajando con mongo o con postgresSql cuidado con eso, se crea el Dto de la paginacion, se hacen modificaciones en el controlador para que reciba los parametros por URL, y se modifica el servicio para que haga la consulta a la base de datos por paginacion

---
+++ src/common/common.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PaginationDto } from './dto/pagination.dto';
+
+
+@Module({
+  controllers: [],
+  providers: [],
+  exports:[
+  ]
+})
+export class CommonModule {}
