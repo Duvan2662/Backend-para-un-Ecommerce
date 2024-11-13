6e73b7491d9579a204b378604ac1934d2adb4b08
Duvan <duvanriano2626@gmail.com>
2024-11-13 09:48:09 -0500

    Cambiar el email por el id en el payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -34,7 +34,7 @@ export class AuthService {
       delete user.password;
       return {
         ...user,
-        token: this.getJwtToken({email:user.email})
+        token: this.getJwtToken({id:user.id})
       };
 
     } catch (error) {
@@ -47,7 +47,7 @@ export class AuthService {
       const {password, email} = loginUserDto;
       const user = await this.userRepository.findOne({
         where:{email},
-        select:{email:true, password:true}
+        select:{email:true, password:true, id: true}
       })
       if (!user) {
         throw new UnauthorizedException('Credentials are not valid (email)');
@@ -58,7 +58,7 @@ export class AuthService {
       }
       return {
         ...user,
-        token: this.getJwtToken({email:user.email})
+        token: this.getJwtToken({id:user.id})
       };
 
     } catch (error) {
